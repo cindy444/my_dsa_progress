dd82b719cf176b89cb4f99119770bf655bceeb81
Cindy Lam <cindybinh444@gmail.com>
2024-07-29 19:56:55 -0700

    feat: added follow up approach for is anagram

--- Array_And_String/Easy/isAnagram.js
+++ Array_And_String/Easy/isAnagram.js
@@ -18,10 +18,6 @@ O(logN)
 sorted and then do comparison between two strings 
 
 after sorting all the unicode will be pushed to the beginning 
-
-One way: 
-isValidChar - to check if each character after sorting and once we find a
-character then discard all the unicode chars
  */
 
 /**
@@ -37,4 +33,46 @@ var isAnagram = function (s, t) {
 
 console.log(isAnagram("anagram", "nagaram")); //expect true
 console.log(isAnagram("rat", "car")); //expect false
-console.log(isAnagram("ra#$>t", "car")); //expect true #TODO: followup question!!
+
+const convertToLowerCaseAlphabetList = (list) => {
+  //iterate through the list
+  //if the character is  not larger than 'z' and smaller than 'a'
+  //which means it is in between 'a' and 'z'
+  //then push it to newList and return it
+  let newList = [];
+  for (let i = 0; i < list.length; i++) {
+    if (!(list[i] > "z" || list[i] < "a")) {
+      newList.push(list[i]);
+    }
+  }
+  return newList;
+};
+
+/**
+ 
+### Follow up question: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+unicode characters -- numerical codes based on the character encoding scheme. When comparing characters, numeric codes/values are used ot determine the order. 
+
+ASCII: 
+'a' > 'A' --> true
+'$' < 'A' --> true
+
+isValidChar - to check if each character after sorting and once we find a
+character then discard all the unicode chars. There are two ways: 
+
+(1) RegEx 
+(2) Iterate through the array of strings and then removed those that are non-Ascii characters, then sorted and join. 
+
+ */
+
+var isAnagramExcludeUnicode = function (s, t) {
+  const sortedS = convertToLowerCaseAlphabetList(s.split("")).sort().join("");
+  const sortedT = convertToLowerCaseAlphabetList(t.split("")).sort().join("");
+  return sortedS === sortedT;
+};
+
+console.log(
+  isAnagramExcludeUnicode("aKK##na$$$$$907gram", "nagaERTWERT#$%#$ram")
+); //expect true
+console.log(isAnagramExcludeUnicode("ABCra#$>t", "car")); //expect false
