cc3da6ba82eac0164af3217dce93880ac1a95e4b
Cindy Lam <cindybinh444@gmail.com>
2024-08-07 22:48:38 -0700

    feat: added moving zeros approach

---
+++ Array_And_String/Easy/movingZeros.js
@@ -0,0 +1,47 @@
+/**
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+
+
+0,1,0,3,12,0
+^
+slice this number 
+
+ */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i], i);
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+    }
+    console.log(i);
+  }
+  return nums;
+};
+
+console.log(moveZeroes([0,1,0,3,12])); //[1,3,12,0,0]
+console.log(moveZeroes([1, 0, 0, 3, 12])); //[1,3,12,0,0] <-- this case is not yet solved. 
+console.log(moveZeroes([0])); //[0]
