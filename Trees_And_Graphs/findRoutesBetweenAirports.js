8139e8d78473fc6a5777e10669e6e3d548f2055c
Cindy Lam <cindybinh444@gmail.com>
2024-08-19 23:14:55 -0700

    feat: added updates on airport routes

--- Trees_And_Graphs/findRoutesBetweenAirports.js
+++ Trees_And_Graphs/findRoutesBetweenAirports.js
@@ -21,6 +21,57 @@ routes A
              / 
            EWR
 
+stack = [{
+    dest: "BOM",
+    price: 180
+  }, {
+    dest: "NYC",
+    price: 300
+  }]
+
+Lets set the input as (start, destination) 
+- start will contain the airport to start flying from
+- destination will contain the airpot to start flying to
+
+-> do we find a starting route of "NYC", set price to 300, if NYC is found as the starting route in the map, 
+if EWR is matching up with the input destination
+  add price 300 + 450 --> compare/update total min price  
+then add EWR dest to the stack
+
+stack = [{
+    dest: "BOM",
+    price: 180
+  }, {
+    dest: "NYC",
+    price: 300
+  },
+  {
+    dest: "EWR", 
+    price: 450
+  }]
+
+
+
+iterate through all the routes in A 
+map 
+dest = destination
+{
+  "LAX": [{
+    dest: "EWR",
+    price: 690
+  }, {
+    dest: "BOM",
+     price: 180
+  }, {
+    dest: "NYC",
+    price: 300
+  }],
+  "NYC": [{
+    dest: "EWR", 
+    price: 450
+  }]
+}
+
 
 there are two possible routes to get from LAX to EWR 
 1. LAX => EWR
@@ -30,7 +81,47 @@ Now we need to know how much does it cost for these routes and then return the c
 
 Apporach: DFS (with stack)
 
+*/
 
+//Problem 1
+
+const ROUTES = {
+  LAX: [
+    {
+      dest: "EWR",
+      price: 690,
+    },
+    {
+      dest: "BOM",
+      price: 180,
+    },
+    {
+      dest: "NYC",
+      price: 300,
+    },
+  ],
+  NYC: [
+    {
+      dest: "EWR",
+      price: 450,
+    },
+  ],
+};
+
+// we need min price
+// we need a route with min price
+// if a matching destination is not found, then we will keep adding the route up, if a start has no "start" key found in the Map object, then, stop adding route, clean it up into blank, set temp price to blank too.
+// only when we see a matching destination, then we will add the price and compare with min price and save the cheapest route found
+// if the price is lower than min price, update the min price and update the cheapeat route to the new cheaper route.
+
+const findACheapestRoute = (routes, start, destination) => {
+
+
+};
+
+console.log(findACheapestRoute(ROUTES, "LAX", "EWR"));
+
+/*
 
 Problem 2: 
 Find all the routes from LAX and EWR
@@ -45,5 +136,3 @@ Apporach: DFS (with stack)
 
 
 */
-
-//Problem 1
